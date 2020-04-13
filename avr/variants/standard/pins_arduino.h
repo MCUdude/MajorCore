237a20250a4090dd820b37b2faa296c41d5927e4
MCUdude <hansibull@gmail.com>
2020-04-13 12:39:19 +0200

    Add PIN macros

--- avr/variants/standard/pins_arduino.h
+++ avr/variants/standard/pins_arduino.h
@@ -200,6 +200,42 @@ const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] =
 	_BV(2)
 };
 
+#define PIN_PB0 0
+#define PIN_PB1 1
+#define PIN_PB2 2
+#define PIN_PB3 3
+#define PIN_PB4 4
+#define PIN_PB5 5
+#define PIN_PB6 6
+#define PIN_PB7 7
+#define PIN_PD0 8
+#define PIN_PD1 9
+#define PIN_PD2 10
+#define PIN_PD3 11
+#define PIN_PD4 12
+#define PIN_PD5 13
+#define PIN_PD6 14
+#define PIN_PD7 15
+#define PIN_PC0 16
+#define PIN_PC1 17
+#define PIN_PC2 18
+#define PIN_PC3 19
+#define PIN_PC4 20
+#define PIN_PC5 21
+#define PIN_PC6 22
+#define PIN_PC7 23
+#define PIN_PA0 24
+#define PIN_PA1 25
+#define PIN_PA2 26
+#define PIN_PA3 27
+#define PIN_PA4 28
+#define PIN_PA5 29
+#define PIN_PA6 30
+#define PIN_PA7 31
+#define PIN_PE0 32
+#define PIN_PE1 33
+#define PIN_PE2 34
+
 #if defined(__AVR_ATmega8515__)
 const uint8_t PROGMEM digital_pin_to_timer_PGM[] =
 {
